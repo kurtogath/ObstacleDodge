f464791ae6316e3c11452d726e4fe827a567f245
Oscar Marti <oscarmartitech@gmail.com>
2025-12-01 19:53:21 +0100

    Created a simple scenario

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -14,7 +14,7 @@ public class Mover : MonoBehaviour
     void Update()
     {
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float yValue = 0f;//Input.GetAxis("Jump");
+        float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Translate(xValue, yValue, zValue);
     }
